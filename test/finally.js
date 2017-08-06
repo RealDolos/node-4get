3bbcc8591fe63486103e3b22af6770e9617a7ac5
RealDolos <dolos@cock.li>
2017-08-06 18:45:41 +0200

    finally support non-callables

--- test/finally.js
+++ test/finally.js
@@ -24,8 +24,17 @@ describe("Promise.finally", () => {
   it("resolve", () => {
     return O().finally(N).then(e => e.should.equal("ok"));
   });
+  it("resolve other p", () => {
+    return O().finally(O("nok")).then(e => e.should.equal("ok"));
+  });
   it("reject", () => {
     return R().finally(N).catch(e => e.should.equal("ok"));
+  });
+  it("reject other p", () => {
+    return R().finally(O("nok")).catch(e => e.should.equal("ok"));
+  });
+  it("reject other plain", () => {
+    return R().finally("ok").catch(e => e.should.equal("ok"));
   });
   it("throw", () => {
     return O().finally(N).catch(e => e.should.equal("ok"));
@@ -46,6 +55,12 @@ describe("Promise.finally", () => {
   });
   it("reject and finally throws", () => {
     return R("nok").finally(T).catch(e => e.should.equal("ok"));
+  });
+  it("reject and finally rejects w/ promise", () => {
+    return R("nok").finally(R()).catch(e => e.should.equal("ok"));
+  });
+  it("reject and finally plain", () => {
+    return R("ok").finally("nok").catch(e => e.should.equal("ok"));
   });
 });
 
